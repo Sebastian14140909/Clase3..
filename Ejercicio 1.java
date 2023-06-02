476e9e56bbbf9d9ba4a8f7c56ec9e07945ca4183
Sebastian14140909 <133933733+Sebastian14140909@users.noreply.github.com>
2023-06-01 21:06:50 -0600

    Add files via upload

---
+++ Ejercicio 1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicio1.java;
+/
+ */
+public class SimbolosEjercicio1Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            for  (int j = 0; j < 10; j++) {
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+    }
+    
+}
